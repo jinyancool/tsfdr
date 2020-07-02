3acf4f0bb869ed39c22cba4349e088e0d72bebf9
Jinyan <jinyancool@163.com>
2020-07-02 09:24:32 +0800

    Lazy commit

--- inst/scripts/installmosek.R
+++ inst/scripts/installmosek.R
@@ -6,21 +6,15 @@ installmosek <- function(){
   if(!"Rmosek" %in% installed.packages()[,"Package"]) {
     install.packages(new.packages)
   }
-  os <- Sys.info()[['sysname']]
-  if (os == "Darwin"){
-    mosekUrl <- "https://download.mosek.com/stable/9.2.14/mosektoolsosx64x86.tar.bz2"
-    download.file(mosekUrl, destfile = file.path(targetDir,"mosektoolsosx64x86.tar.bz2"))
-    cmd <- sprintf("cd ~/bin && tar -xvf mosektoolsosx64x86.tar.bz2")
-    system(cmd)
-    Rmosek::mosek_attachbuilder("~/bin/mosek/9.2/tools/platform/osx64x86/bin")
-  }
-  if (os == "Linux"){
-    mosekUrl <- "https://download.mosek.com/stable/9.2.14/mosektoolslinux64x86.tar.bz2"
-    download.file(mosekUrl, destfile = file.path(targetDir,"mosektoolslinux64x86.tar.bz2"))
-    cmd <- sprintf("cd ~/bin && tar -xvf mosektoolslinux64x86.tar.bz2")
-    system(cmd)
-    Rmosek::mosek_attachbuilder("~/bin/mosek/9.2/tools/platform/linux64x86/bin")
-  }
+  switch(Sys.info()[['sysname']],
+         Windows= {ostype <- "windows"},
+         Linux  = {ostype <- "linux"},
+         Darwin = {ostype <- "osx"})
+  mosekUrl <- sprintf("https://download.mosek.com/stable/9.2.14/mosektools%s64x86.tar.bz2", ostype)
+  download.file(mosekUrl, destfile = sprintf("%s/mosektools%s64x86.tar.bz2", targetDir, ostype))
+  cmd <- sprintf("cd %s && tar -xvf mosektools%s64x86.tar.bz2", targetDir, ostype)
+  system(cmd)
+  Rmosek::mosek_attachbuilder(sprintf("%s/mosek/9.2/tools/platform/%s64x86/bin", targetDir, ostype))
   install.rmosek()
   if(!file.exists("~/mosek/mosek.lic")){
     message("Please visit: https://www.mosek.com/products/academic-licenses/ to get the licsense.
